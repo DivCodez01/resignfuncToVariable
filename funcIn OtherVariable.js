737af87c4603d40d0fdc494a9b2a21a51b65caee
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-19 19:49:24 +0200

    useMath

--- funcIn OtherVariable.js
+++ funcIn OtherVariable.js
@@ -10,7 +10,7 @@ console.log(number1 + number2); */
 
 // resign the different value
 
-let number1 = numberOne();
+/* let number1 = numberOne();
 let number2 = numberTwo();
 
 function numberOne() {
@@ -20,5 +20,19 @@ function numberTwo() {
     return 40;
 }
 
+console.log(number1 + number2); */
+
+// use math to change value and number coming from 2 up to 7
+
+let number1 = number();
+let number2 = number();
+
+function number() {
+    return Math.floor(Math.random() * 7) + 2
+}
+console.log(number1)
+console.log(number2)
 console.log(number1 + number2);
+
+
 
