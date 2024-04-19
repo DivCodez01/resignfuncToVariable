98280e586154aa68221e99e0ff3a06f10972789a
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-19 19:43:31 +0200

    changeValue

--- funcIn OtherVariable.js
+++ funcIn OtherVariable.js
@@ -1,7 +1,24 @@
-let number1 = signNumber();
+// resign the variable the same value
+
+/* let number1 = signNumber();
 let number2 = signNumber();
 
 function signNumber() {
     return 30;
 }
+console.log(number1 + number2); */
+
+// resign the different value
+
+let number1 = numberOne();
+let number2 = numberTwo();
+
+function numberOne() {
+    return 30;
+}
+function numberTwo() {
+    return 40;
+}
+
 console.log(number1 + number2);
+
