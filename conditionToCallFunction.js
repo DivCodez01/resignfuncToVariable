fdb4e415171ed0dfec5a61a01327c317c68b35e7
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-22 18:21:14 +0200

    whenIsInsufficient

--- conditionToCallFunction.js
+++ conditionToCallFunction.js
@@ -1,5 +1,8 @@
 document.title = "Use condition to call function";
 
+// account is more than $ needed
+
+/*
 let money = 200;
 let charge = 150;
 let current = "$";
@@ -7,6 +10,24 @@ if (money >= charge) {
     ifFunction();
 } else {
     console.log("Your balance is insufficient!");
+};
+
+function ifFunction() {
+    let left = "Your account left is: ";
+    console.log(`${left} ${money - charge}${current}`)
+}
+
+*/
+
+// when your account comes down
+
+let money = 100;
+let charge = 150;
+let current = "$";
+if (money >= charge) {
+    ifFunction();
+} else {
+    console.log(`Your balance is insufficient! The ${current} left to add is ${charge - money}${current}`);
 };
 
 function ifFunction() {
